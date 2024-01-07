89559c177277660dd0adcde6badd6c1824966d0c
hashamkhaan <hashamkhancust@gmail.com>
2024-01-07 21:41:31 +0500

    Baggage Models Added

---
+++ src/modules/pnr/extraBaggage/extraBaggage.entity.ts
@@ -0,0 +1,45 @@
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  ForeignKey,
+  BelongsTo,
+} from 'sequelize-typescript';
+
+import { PnrBooking } from '../../pnr/pnrBooking/entities/pnrBooking.entity';
+
+@Table
+export class ExtraBaggage extends Model {
+  @Column({
+    type: DataType.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+  })
+  localId: number;
+  @Column({
+    type: DataType.BIGINT,
+  })
+  id: number;
+
+  @ForeignKey(() => PnrBooking)
+  @Column({
+    type: DataType.BIGINT,
+    allowNull: false,
+    onDelete: 'NO ACTION',
+  })
+  pnrBookingId: number;
+  // Start
+
+  @Column
+  unit: string;
+
+  @Column(DataType.DOUBLE)
+  weight: number;
+
+  // End
+  @BelongsTo(() => PnrBooking)
+  pnrBooking: PnrBooking;
+}
+
+export default ExtraBaggage;
