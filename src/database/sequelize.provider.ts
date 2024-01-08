5349ab20026dd5a89343637c480a0dfab7171617
hashamkhaan <hashamkhancust@gmail.com>
2024-01-09 00:11:04 +0500

    Fix console error message formatting

--- src/database/sequelize.provider.ts
+++ src/database/sequelize.provider.ts
@@ -72,7 +72,7 @@ sequelize
   })
   .catch((error) => {
     console.error('Error synchronizing the database:', error);
-    console.error('-------------------------------:', error.message);
+    console.error('--------------------------------:', error.message);
   });
 
 export { sequelize, Transaction, Op };
