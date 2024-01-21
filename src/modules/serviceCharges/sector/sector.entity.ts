995c8dc74a305cfa737ee692f5b0881331c0de46
hashamkhaan <hashamkhancust@gmail.com>
2024-01-21 21:54:53 +0500

    commissionCategories.entity.ts and index.ts

---
+++ src/modules/serviceCharges/sector/sector.entity.ts
@@ -0,0 +1,26 @@
+import { Table, Column, Model, DataType } from 'sequelize-typescript';
+
+@Table
+export class Sector extends Model {
+  @Column({
+    type: DataType.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+  })
+  id: number;
+
+  @Column
+  name: string;
+  @Column
+  description: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  description: code;
+
+  // End
+}
+
+export default Sector;
