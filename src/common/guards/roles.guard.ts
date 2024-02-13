5dd06c57fe25e7113f0daa90a5f150006a15a8d2
hashamkhaan <hashamkhancust@gmail.com>
2024-02-13 19:14:07 +0500

    Same

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -29,7 +29,6 @@ export class RolesGuard implements CanActivate {
       return true;
     }
     const { user } = context.switchToHttp().getRequest();
-    console.log('-----------------user', user, requiredGuardParams);
     if (!user) {
       return false;
     }
