de42fa3607ea926ed31e3ffb8fe1dabc065994dd
hashamkhaan <hashamkhancust@gmail.com>
2024-02-22 02:34:00 +0500

    Update PnrPayment entity with new columns

--- src/modules/paymentModules/paymob/entities/pnrPayment.entity.ts
+++ src/modules/paymentModules/paymob/entities/pnrPayment.entity.ts
@@ -159,7 +159,7 @@ export class PnrPayment extends Model {
   refunded_amount_cents: number;
 
   @Column({
-    type: DataType.INTEGER,
+    type: DataType.DOUBLE,
     allowNull: true,
   })
   captured_amount: number;
@@ -229,6 +229,155 @@ export class PnrPayment extends Model {
     allowNull: true,
   })
   txn_response_code: number;
+
+  // New
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  })
+  is_payment_locked: boolean;
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  })
+  is_return: boolean;
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  })
+  is_cancel: boolean;
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  })
+  is_returned: boolean;
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  })
+  is_canceled: boolean;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+    defaultValue: true,
+  })
+  merchant_order_id: number;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    defaultValue: true,
+  })
+  wallet_notification: string;
+
+  @Column({
+    type: DataType.DOUBLE,
+    allowNull: false,
+    defaultValue: true,
+  })
+  paid_amount_cents: number;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  })
+  notify_user_with_email: boolean;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    defaultValue: true,
+  })
+  order_url: string;
+
+  @Column({
+    type: DataType.DOUBLE,
+    allowNull: false,
+    defaultValue: true,
+  })
+  commission_fees: number;
+
+  @Column({
+    type: DataType.DOUBLE,
+    allowNull: false,
+    defaultValue: true,
+  })
+  delivery_fees_cents: number;
+
+  @Column({
+    type: DataType.DOUBLE,
+    allowNull: false,
+    defaultValue: true,
+  })
+  delivery_vat_cents: number;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+  })
+  payment_method: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+  })
+  merchant_staff_tag: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+  })
+  api_source: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+  })
+  created_at_original: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+  })
+  updated_at_original: string;
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: true,
+  })
+  terminal_id: number;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: true,
+  })
+  is_hidden: boolean;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: true,
+  })
+  error_occured: boolean;
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: true,
+  })
+  is_live: boolean;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+    defaultValue: true,
+  })
+  source_id: number;
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  })
+  is_captured: boolean;
+  // New End
 
   @Column({
     type: DataType.STRING,
