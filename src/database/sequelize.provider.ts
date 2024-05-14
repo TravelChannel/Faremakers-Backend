4d77054024def4e77fcc099c20958326a975cbc9
hashamkhaan <hashamkhancust@gmail.com>
2024-05-14 11:11:26 +0500

    chore: Remove force option from sequelize sync

--- src/database/sequelize.provider.ts
+++ src/database/sequelize.provider.ts
@@ -107,10 +107,10 @@ sequelize.addModels([
 // Test
 // Sync the models with the database, dropping and recreating tables
 sequelize
-  .sync({
-    force: true,
-  })
-  // .sync()
+  // .sync({
+  //   force: true,
+  // })
+  .sync()
   .then(() => {
     console.log('Database synchronized...');
   })
