3293a8d7e7c5a00ad9c25a2e86c2dc6c96b581ca
MUHAMMAD NABEEL <nabeelfarooq123456@gmail.com>
2024-11-11 15:07:15 +0500

    Update Masterprice controller

--- src/modules/masterPriceTravelBoard/masterprice.controller.ts
+++ src/modules/masterPriceTravelBoard/masterprice.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { MasterPriceService } from './masterprice.service';
+import { SkipAuth } from 'src/common/decorators/skip-auth.decorator';
 
 @Controller('masterprice')
 export class MasterPriceController {
   constructor(private readonly soapService: MasterPriceService) {}
   @Post('generate-envelope')
+  @SkipAuth()
   async generateSOAPEnvelope(@Body() requestData: any): Promise<string> {
     return await this.soapService.buildSOAPEnvelope(requestData);
   }
