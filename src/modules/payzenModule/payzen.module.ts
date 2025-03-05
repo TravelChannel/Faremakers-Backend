e0eb514259d57947664d610b1dda7db609cda71f
MUHAMMAD NABEEL <nabeelfarooq123456@gmail.com>
2025-03-05 10:58:16 +0500

    Data Inserted For Payzen

--- src/modules/payzenModule/payzen.module.ts
+++ src/modules/payzenModule/payzen.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { PayzenController } from './payzen.controller';
 import { PayzenService } from './payzen.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { PayZenOrder } from './entities/payzen-order.entity';
 
 @Module({
   imports: [
-    HttpModule, // Import HttpModule for HTTP requests
+    SequelizeModule.forFeature([PayZenOrder]),
+    HttpModule.register({}), // Register HttpModule properly
   ],
+
   controllers: [PayzenController],
   providers: [PayzenService], // Provide the PayzenService
 })
